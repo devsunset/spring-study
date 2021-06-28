2891a1f5d560d0713628c4abe9911df31fd60f56
devsunset <devsunset@gmail.com>
2021-06-28 16:07:02 +0900

    dev

--- src/main/java/com/example/springwork/controller/HelloRestfulController.java
+++ src/main/java/com/example/springwork/controller/HelloRestfulController.java
@@ -1,5 +1,7 @@
 package com.example.springwork.controller;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.concurrent.atomic.AtomicLong;
 
 import com.example.springwork.domain.Hello;
@@ -17,5 +19,24 @@ public class HelloRestfulController {
 	@GetMapping("/helloRestful")
 	public Hello hello(@RequestParam(value = "name", defaultValue = "World") String name) {
 		return new Hello(counter.incrementAndGet(), String.format(template, name+" - RestController RESTful (use Jackson &  return json)"));
+	}
+
+	@GetMapping("/helloRestfulSub")
+	public Object hellosub(@RequestParam(value = "name", defaultValue = "World") String name) {
+		
+		ArrayList<HashMap<String,String>> dummyArrayList = new ArrayList<HashMap<String,String>>();
+		HashMap<String,String> dummyHashMap = new HashMap<String,String>();
+		dummyHashMap.put("1", "A");
+		dummyHashMap.put("2", "B");
+		dummyHashMap.put("3", "C");
+		dummyArrayList.add(dummyHashMap);
+
+		dummyHashMap = new HashMap<String,String>();
+		dummyHashMap.put("4", "a");
+		dummyHashMap.put("5", "b");
+		dummyHashMap.put("6", "c");
+		dummyArrayList.add(dummyHashMap);
+
+		return dummyArrayList;
 	}
 }
