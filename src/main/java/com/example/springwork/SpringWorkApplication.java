28cfedb02457de1a4d958afa042b7d6e6e145f71
devsunset <devsunset@gmail.com>
2021-06-28 14:49:10 +0900

    dev

--- src/main/java/com/example/springwork/SpringWorkApplication.java
+++ src/main/java/com/example/springwork/SpringWorkApplication.java
@@ -18,12 +18,14 @@ public class SpringWorkApplication {
 	@Bean
 	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
 		return args -> {
+			System.out.println("##################################################");
 			System.out.println("Let's inspect the beans provided by Spring Boot:");
 			String[] beanNames = ctx.getBeanDefinitionNames();
 			Arrays.sort(beanNames);
 			for (String beanName : beanNames) {
 				System.out.println(beanName);
 			}
+			System.out.println("##################################################");
 		};
 	}
 }
