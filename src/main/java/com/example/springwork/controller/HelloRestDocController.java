9301f702a9a98bf611c937bed42a8a1abc991654
devsunset <devsunset@gmail.com>
2021-06-29 12:37:04 +0900

    dev

---
+++ src/main/java/com/example/springwork/controller/HelloRestDocController.java
@@ -0,0 +1,18 @@
+package com.example.springwork.controller;
+
+
+import java.util.Collections;
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloRestDocController {
+
+	@GetMapping("/helloRestDoc")
+	public Map<String, Object> greeting() {
+		return Collections.singletonMap("message", "Hello, World");
+	}
+
+}
