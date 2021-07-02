cfc911fceb581e5ac998c7c0546934e5382a4fae
devsunset <devsunset@gmail.com>
2021-07-02 15:15:45 +0900

    dev

---
+++ src/test/java/com/example/springwork/controller/FileSystemStorageServiceTest.java
@@ -0,0 +1,71 @@
+package com.example.springwork.controller;
+
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
+import com.example.springwork.service.FileSystemStorageService;
+import com.example.springwork.support.exception.StorageFileNotFoundException;
+
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.test.web.servlet.MockMvc;
+
+
+
+@AutoConfigureMockMvc
+@SpringBootTest
+public class FileSystemStorageServiceTest {
+
+	@Autowired
+	private MockMvc mvc;
+
+	// @MockBean
+	// private StorageService storageService;
+
+   	@MockBean
+    FileSystemStorageService fileSystemStorageService;
+
+	@Test
+	public void shouldListAllFiles() throws Exception {
+		given(this.fileSystemStorageService.loadAll())
+				.willReturn(Stream.of(Paths.get("first.txt"), Paths.get("second.txt")));
+
+		this.mvc.perform(get("/")).andExpect(status().isOk())
+				.andExpect(model().attribute("files",
+						Matchers.contains("http://localhost/files/first.txt",
+								"http://localhost/files/second.txt")));
+	}
+
+	@Test
+	public void shouldSaveUploadedFile() throws Exception {
+		MockMultipartFile multipartFile = new MockMultipartFile("file", "test.txt",
+				"text/plain", "Spring Framework".getBytes());
+		this.mvc.perform(multipart("/uploadForm").file(multipartFile))
+				.andExpect(status().isFound())
+				.andExpect(header().string("Location", "/uploadForm"));
+
+		then(this.fileSystemStorageService).should().store(multipartFile);
+	}
+
+	@Test
+	public void should404WhenMissingFile() throws Exception {
+		given(this.fileSystemStorageService.loadAsResource("test.txt"))
+				.willThrow(StorageFileNotFoundException.class);
+
+		this.mvc.perform(get("/files/test.txt")).andExpect(status().isNotFound());
+	}
+
+}
