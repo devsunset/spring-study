7d85cd821fcd36618663307f4c2efcbdd47465b8
devsunset <devsunset@gmail.com>
2021-07-04 09:46:38 +0900

    security

---
+++ src/main/java/com/example/springwork/support/configuration/MvcConfig.java
@@ -0,0 +1,17 @@
+package com.example.springwork.support.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/home").setViewName("home");
+		// registry.addViewController("/").setViewName("home");
+		registry.addViewController("/hellosecurity").setViewName("hellosecurity");
+		registry.addViewController("/login").setViewName("login");
+	}
+
+}
